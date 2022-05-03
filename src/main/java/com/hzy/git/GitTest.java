d0db1ba29ad6cf13caef72b908bb9c2f87d206fd
hzy2021 <80233100+hzy2021@users.noreply.github.com>
2022-05-03 20:42:16 +0800

    pull commit

--- src/main/java/com/hzy/git/GitTest.java
+++ src/main/java/com/hzy/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix hello git6");
         System.out.println("123 ");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
