6f376b16cddcb6aa9613f234204fa4bb515684a6
huangziyang <527172691@qq.com>
2022-05-03 21:20:46 +0800

    dev-commit2

--- src/main/java/com/hzy/git/GitTest.java
+++ src/main/java/com/hzy/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("dev-test");
+        System.out.println("dev-test2");
     }
 }
