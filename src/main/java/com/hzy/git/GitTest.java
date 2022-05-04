cdc5ddf2a129c75584536a3830415b43b0c95b91
huangziyang <huangziyang06@meituan.com>
2022-05-04 10:36:37 +0800

    mac dev-test2

--- src/main/java/com/hzy/git/GitTest.java
+++ src/main/java/com/hzy/git/GitTest.java
@@ -6,10 +6,5 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("master hello git5");
-        System.out.println("hot-fix hello git6");
-        System.out.println("123 ");
-        System.out.println("push test");
-        System.out.println("pull test");
     }
 }
