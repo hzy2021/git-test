14a19827e9adfa019230e4cb1ba1957f2f580a94
huangziyang <huangziyang06@meituan.com>
2022-05-04 10:22:56 +0800

    mac dev-test1

--- src/main/java/com/hzy/git/GitTest.java
+++ src/main/java/com/hzy/git/GitTest.java
@@ -13,5 +13,8 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("dev-test");
         System.out.println("dev-test2");
+        System.out.println("mac dev-test3");
+        System.out.println("mac dev-test4");
+
     }
 }
