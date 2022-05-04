72b9e351d19177557004711af09b22cdd954fb25
hzy2021 <80233100+hzy2021@users.noreply.github.com>
2022-05-04 10:35:16 +0800

    test

--- src/main/java/com/hzy/git/GitTest.java
+++ src/main/java/com/hzy/git/GitTest.java
@@ -6,16 +6,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("master hello git5");
-        System.out.println("hot-fix hello git6");
-        System.out.println("123 ");
-        System.out.println("push test");
-        System.out.println("pull test");
-        System.out.println("dev-test");
-        System.out.println("dev-test2");
-        System.out.println("mac dev-test3");
-        System.out.println("mac dev-test4");
-        
-        System.out.println("mac pull-test2");
+        System.out.println("mac pull-test");
     }
 }
