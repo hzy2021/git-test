220f70fa1ddfabfa6f779e266d0f82747d06bd41
hzy2021 <80233100+hzy2021@users.noreply.github.com>
2022-05-04 10:56:28 +0800

    Update GitTest.java

--- src/main/java/com/hzy/git/GitTest.java
+++ src/main/java/com/hzy/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("web-conflict");
         System.out.println("local conflict123");
+        System.out.println("push test");
     }
 }
