7732513f1b67dbad1f706b3546fa9c7c4c61e243
huangziyang <huangziyang06@meituan.com>
2022-05-04 10:53:13 +0800

    mac dev-test

--- src/main/java/com/hzy/git/GitTest.java
+++ src/main/java/com/hzy/git/GitTest.java
@@ -2,12 +2,6 @@ package com.hzy.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("mac pull-test");
-        System.out.println("mac pull-test2");
         System.out.println("web-conflict");
         System.out.println("local conflict123");
     }
